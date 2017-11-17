352a17475e0a4d770bd5ce64608fc1f9c82b97ca
Gurjeet Singh <gurjeets@smartdatainc.net>
2017-11-17 18:52:47 +0530

    Fixed csv read issue

--- sdcPredictor.py
+++ sdcPredictor.py
@@ -1,8 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+
+import io
+import requests
 
 poly_reg = PolynomialFeatures(degree = 4)
 lin_reg2 = LinearRegression()
@@ -22,7 +26,11 @@ class SdcPredictor(object):
 	
 	def importData(self):
 		#importing dataset
-		dataset = pd.read_csv("./data/Position_Salaries.csv")
+
+		url     = "https://raw.githubusercontent.com/gurjeetsdc/sdcml/master/data/Position_Salaries.csv"
+		s       = requests.get(url).content
+		dataset = pd.read_csv(io.StringIO(s.decode('utf-8')))
+		#dataset = pd.read_csv("https://github.com/gurjeetsdc/sdcml/blob/master/data/Position_Salaries.csv")
 
 		X = dataset.iloc[:,1:2].values
 		y = dataset.iloc[:,-1].values
